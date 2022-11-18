ea99d67b03aad6c6d7246959b9050634e890bc96
Ankit kumar <ankitkumar@192.168.1.5>
2022-11-18 17:01:43 +0530

    edit project file to generate the nuget package for the project

--- obj/Debug/net6.0/StudentMicroservice.AssemblyInfo.cs
+++ obj/Debug/net6.0/StudentMicroservice.AssemblyInfo.cs
@@ -10,11 +10,11 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StudentMicroservice")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ABK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("StudentMicroservice")]
+[assembly: System.Reflection.AssemblyProductAttribute("Student Microservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
